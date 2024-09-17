ee7c968333bbc286096bbe75c0610befaa72201c
Eng Amr <amramer11111998@gmail.com>
2024-09-18 01:18:19 +0300

    add localizations

---
+++ lib/constant/strings.dart
@@ -0,0 +1,41 @@
+import 'package:easy_localization/easy_localization.dart';
+
+class Strings {
+  Strings._();
+
+  static Strings get instance => Strings._();
+
+  String get settings => 'settings'.tr();
+
+  String get newsApp => 'newsApp'.tr();
+
+  String get categories => 'Categories'.tr();
+
+  String get search => 'search'.tr();
+
+  String get somethingWentWrong => 'somethingWentWrong'.tr();
+
+  String get sports => 'sports'.tr();
+
+  String get general => 'general'.tr();
+
+  String get health => 'health'.tr();
+
+  String get business => 'business'.tr();
+
+  String get entertainment => 'entertainment'.tr();
+
+  String get science => 'science'.tr();
+
+  String get tryAgain => 'tryAgain'.tr();
+
+  String get pickYourCategoryOfInterest => 'pickYourCategoryOfInterest'.tr();
+
+  String get viewFullArticle => 'viewFullArticle'.tr();
+
+  String get language => 'language'.tr();
+
+  String get english => 'english'.tr();
+
+  String get arabic => 'arabic'.tr();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:news/core/theme/theme.dart';
+import 'package:news/constant/theme.dart';
 import 'package:news/pages/home_screen.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   Future.delayed(const Duration(milliseconds: 500));
   FlutterNativeSplash.remove();
-  runApp(const MyApp());
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      path: 'assets/localization',
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,17 +29,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: Size(412.w, 870.h),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: HomeScreen.routeName,
-        routes: {
-          HomeScreen.routeName: (context) => HomeScreen(),
-          //  CategoryDetails.routeName: (context) =>  CategoryDetails(),
-        },
-        theme: MyTheme.lightTheme,
-      ),
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+        //  CategoryDetails.routeName: (context) =>  CategoryDetails(),
+      },
+      theme: MyTheme.lightTheme,
     );
   }
 }
