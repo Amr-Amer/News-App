dfdb9daece6c91cb3a6b671202544849138fff53
Eng Amr <amramer11111998@gmail.com>
2024-09-17 03:28:56 +0300

    add ScreenUtilInit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news/core/theme/theme.dart';
 import 'package:news/pages/home_screen.dart';
 
@@ -14,17 +15,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName,
-      routes: {
-        HomeScreen.routeName: (context) => HomeScreen(),
-        //  CategoryDetails.routeName: (context) =>  CategoryDetails(),
-      },
-      theme: MyTheme.lightTheme,
+    return ScreenUtilInit(
+      designSize: Size(412.w, 870.h),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: HomeScreen.routeName,
+        routes: {
+          HomeScreen.routeName: (context) => HomeScreen(),
+          //  CategoryDetails.routeName: (context) =>  CategoryDetails(),
+        },
+        theme: MyTheme.lightTheme,
+      ),
     );
   }
 }
